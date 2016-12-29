73cc40e5297c37bd0aebfd2c932788816d4b4ed7
fasthro <chenchaoflying@outlook.com>
2016-12-29 21:50:51 +0800

    添加花椒总页数逻辑

---
+++ huajiao.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# @Author: fasthro
+# @Date:   2016-12-29 21:14:57
+# @Last Modified by:   fasthro
+# @Last Modified time: 2016-12-29 21:14:57
+
+import urllib2
+import requests
+import re
+from lxml import etree
+import bs4
+
+
+'''
+ 抓取花椒直播网主播个人相关数据
+'''
+
+
+class Huajiao():
+    def __init__(self):
+        pass
+
+    def get_hot(self):
+        '''
+        抓取热门推荐
+        :return:
+        '''
+
+        t_url = 'http://www.huajiao.com/category/1000'
+        parme = '?pageno=%s'
+        page_total = 0
+
+        # 分析总页数
+        page_total = self.get_total_page(t_url)
+
+        #分析每一页的具体数据
+
+
+    def get_total_page(self, url):
+        '''
+        获取目标总页数
+        :param url:
+        :return:
+        '''
+        num_html = requests.get(url).content
+        num_tree = etree.HTML(num_html).xpath('.//div[@id="doc-bd"]//div[@class="container clearfix"]//ul/li')
+        if len(num_tree) > 0:
+            num_env = num_tree[len(num_tree) - 1]
+            page_total = num_env.get("tabindex")
+            return page_total
+        return 1
+
+if __name__ == "__main__":
+    client = Huajiao()
+    client.get_hot();
